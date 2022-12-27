dadd65765ba51365c74c3573765581a9530f0a09
msyavuz <salih.yavuz@metu.edu.tr>
2022-12-27 18:23:32 +0300

    responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -30,16 +30,16 @@ function App() {
     }, []);
 
     return (
-        <div className="App h-screen flex items-center justify-center flex-col bg-orange-700">
+        <div className="App h-screen flex items-center justify-center flex-col bg-orange-700 overflow-hidden">
             <div
-                className="max-h-7xl max-w-7xl flex flex-col items-center justify-center gap-16 bg-white px-10 py-20 rounded"
+                className="flex flex-col flex-wrap items-center justify-center gap-16 bg-white rounded p-10"
                 data-testid="quote-box"
                 id="quote-box"
             >
                 <div
                     data-testid="text"
                     id="text"
-                    className="text-4xl text-red-500"
+                    className="text-xl sm:text-4xl text-red-500"
                 >
                     <FaQuoteLeft className="text-red-500" />
                     {quote.quote}
@@ -47,16 +47,16 @@ function App() {
                 <div
                     data-testid="author"
                     id="author"
-                    className="text-xl text-red-500"
+                    className="sm:text-xl text-red-500"
                 >
                     {quote.author}
                 </div>
-                <div className="buttons-container flex gap-60 justify-center items-center text-xl">
+                <div className="buttons-container flex gap-20 sm:gap-60 justify-center items-center sm:text-xl">
                     <a
                         href={`https://twitter.com/intent/tweet?text=${quote.quote}`}
                         id="tweet-quote"
                         onClick={handleTweetClick}
-                        className="text-center font-bold text-white text-3xl bg-red-500 hover:bg-red-900 px-2 py-2 rounded"
+                        className="text-center font-bold text-white sm:text-3xl bg-red-500 hover:bg-red-900 px-2 py-2 rounded"
                     >
                         <BsTwitter />
                     </a>
