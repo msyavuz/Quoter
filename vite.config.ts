660498083503a7ed3a8c4dc4708e9deffb595431
msyavuz <salih.yavuz@metu.edu.tr>
2022-12-27 17:36:48 +0300

    vite-config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from "vite";
+import type { UserConfig as VitestUserConfigInterface } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
+const vitestConfig: VitestUserConfigInterface = {
     test: {
         globals: true,
         environment: "jsdom",
         setupFiles: "./src/tests/setup.js",
     },
+};
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    base: "/Quoter/",
+    plugins: [react()],
+    test: vitestConfig.test,
 });
